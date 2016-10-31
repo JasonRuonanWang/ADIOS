0d0d7f54e7b7dd52e6c7b76e9fb7c12fe1a84126
Jason Wang <wangr1@ornl.gov>
2016-10-31 16:44:44 -0400

    added dataman to configure.am

--- src/write/adios_dataman.c
+++ src/write/adios_dataman.c
@@ -3,32 +3,71 @@
 #include "core/adios_transport_hooks.h"
 #include "public/adios_types.h"
 
+void *so = 0;
+void (*write_func)(const void *data) = 0;
+void (*init_func)() = 0;
+int (*open_func)() = 0;
+
+void check_library(){
+    if (!so) so=dlopen("./libdataman.so",RTLD_NOW);
+    if (!so) printf("libdataman.so not found\n");
+
+    if (!init_func) init_func=dlsym(so,"dataman_init_c");
+    if (!init_func) printf("dataman_init_c not found in libdataman.so\n");
+    if (!open_func) open_func=dlsym(so,"dataman_open_c");
+    if (!open_func) printf("dataman_open_c not found in libdataman.so\n");
+    if (!write_func) write_func=dlsym(so,"dataman_write_c");
+    if (!write_func) printf("dataman_write_c not found in libdataman.so\n");
+}
+
+
+void adios_dataman_init (const PairStruct * parameters
+                      ,struct adios_method_struct * method
+                      )
+{
+    printf("adios_dataman.c: adios_dataman_init \n");
+    check_library();
+    init_func();
+}
+
+int adios_dataman_open (
+        struct adios_file_struct * fd,
+        struct adios_method_struct * method,
+        MPI_Comm comm)
+{
+    printf("adios_dataman.c: adios_dataman_open \n");
+    check_library();
+    return open_func();
+}
+
+enum BUFFERING_STRATEGY adios_dataman_should_buffer (struct adios_file_struct * fd
+                                                  ,struct adios_method_struct * method
+                                                  )
+{
+    return no_buffering;
+}
+
 void adios_dataman_write(
     struct adios_file_struct *fd,
     struct adios_var_struct *f,
     const void *data,
     struct adios_method_struct *method)
 {
-    void *myso = dlopen("./libdataman.so", RTLD_NOW);
-    void (*func)(struct adios_file_struct*, struct adios_var_struct*, const void*, struct adios_method_struct*)
-        = dlsym(myso, "adios_dataman_write");
-    func(fd, f, data, method);
-    dlclose(myso);
+    printf("adios_dataman.c: adios_dataman_write \n");
+    check_library();
+    write_func(data);
 }
 
-int adios_dataman_open (struct adios_file_struct * fd
-                     ,struct adios_method_struct * method, MPI_Comm comm
-                     )
+void adios_dataman_close (struct adios_file_struct * fd
+                       ,struct adios_method_struct * method
+                       )
 {
-    printf("adios_dataman_open \n");
-    return 0;
 }
 
-void adios_dataman_init (const PairStruct * parameters
-                      ,struct adios_method_struct * method
-                      )
+void adios_dataman_finalize (int mype, struct adios_method_struct * method)
 {
-    printf("adios_dataman_init \n");
+    printf("adios_dataman_finalize \n");
+    if (so) dlclose(so);
 }
 
 void adios_dataman_read (struct adios_file_struct * fd
@@ -47,18 +86,6 @@ void adios_dataman_buffer_overflow (struct adios_file_struct * fd,
     printf("adios_dataman_buffer_overflow \n");
 }
 
-void adios_dataman_close (struct adios_file_struct * fd
-                       ,struct adios_method_struct * method
-                       )
-{
-    printf("adios_dataman_close \n");
-}
-
-void adios_dataman_finalize (int mype, struct adios_method_struct * method)
-{
-    printf("adios_dataman_finalize \n");
-}
-
 void adios_dataman_end_iteration (struct adios_method_struct * method)
 {
     printf("adios_dataman_end_iteration \n");
@@ -72,15 +99,6 @@ void adios_dataman_get_write_buffer (struct adios_file_struct * fd
                                   )
 {
     printf("adios_dataman_get_write_buffer \n");
-}
-
-
-enum BUFFERING_STRATEGY adios_dataman_should_buffer (struct adios_file_struct * fd
-                                                  ,struct adios_method_struct * method
-                                                  )
-{
-    printf("adios_dataman_should_buffer \n");
-    return no_buffering;
 }
 
 void adios_dataman_start_calculation (struct adios_method_struct * method)
